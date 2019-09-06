ce129630a9a7cd6fb7272cbdf0d06a6b05649eec
Luke Lin <lukelin@Lukes-MacBook-Pro-2.local>
2019-09-06 13:08:15 -0400

    init

---
+++ start_dik.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker run -it --rm -p 11200:11200 nycdsa/linux-toolkits
+mkdir /home/ubuntu/Applications
+mkdir /home/ubuntu/Applications/dss
+cd /home/ubuntu/Applications
+wget http://downloads.dataiku.com/public/studio/5.1.5/dataiku-dss-5.1.5.tar.gz
+
+tar xzf dataiku-dss-5.1.5.tar.gz -C ./dss (This one takes some time)
+cd dss
+dataiku-dss-5.1.5/installer.sh -d ./data_directory -p 11200
+
+sudo apt-get update
+sudo apt-get -y install acl curl nginx unzip zip python2.7 libpython2.7 libgfortran3
+./dataiku-dss-5.1.5/installer.sh -d ./data_directory -p 11200
+./data_directory/bin/dss start
